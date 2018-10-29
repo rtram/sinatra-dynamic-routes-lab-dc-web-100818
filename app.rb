83eed1d5a931457ad07065f4724c5f4341a4046d
Robin Tram <robin.tram@gmail.com>
2018-10-29 19:45:51 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,6 +1,26 @@
 require_relative 'config/environment'
+require "pry"
 
 class App < Sinatra::Base
-  # Write your code here!
+  
+  get "/reversename/:name" do 
+    @name = params[:name]
+    "#{@name.reverse}"
+  end 
+  
+  get "/square/:number" do
+    @number = params[:number].to_i
+    "#{@number*@number}"
+  end 
+  
+  get "/say/:number/:phrase" do 
+    @number = params[:number].to_i
+    @phrase = params[:phrase]
+    i = 0
+    while i < @number
+      i += 1
+      resp. "#{@phrase}"
+    end 
+  end 
 
 end
